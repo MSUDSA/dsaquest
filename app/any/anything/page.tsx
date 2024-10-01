dec90a24122b4a28d18fbe02940ce2dbf00a6d83
obwoj1 <149099282+obwoj1@users.noreply.github.com>
2024-10-01 18:10:31 -0400

    dsaquest utils.tsx jsx function

--- app/any/anything/page.tsx
+++ app/any/anything/page.tsx
@@ -1,5 +1,28 @@
+"use client"
+import { useState } from "react"
+
 export default function P() {
+  const [state , setState] = useState<number | string>(0)
+  
+  async function send(){
+    const res =  await fetch("http://localhost:5001/",{
+      method:"GET",
+      headers:{
+        "Content-Type":"application/json"
+      }
+    })
+    const data = await res.json()
+    setState(data.state)
+
+  }
+
   return(
-    <div><p>na</p></div>
+    <>
+    <div><p>{state}</p></div>
+    <button
+    onClick={send}
+    >update
+    </button>
+    </>
   )
 }
---
+++ app/utils.tsx
@@ -0,0 +1,4 @@
+function validateEmail(email: string) {
+    return email.slice(-10) === 'morgan.edu';
+}
+
