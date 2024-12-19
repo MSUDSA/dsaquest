94e1b314e6eff5e8dd6d0795398d902773e0e9a4
Oluwatobi <tobiolajide01@gmail.com>
2024-12-19 13:25:00 -0500

    user models

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,2 +1,16 @@
 from app import db
 
+class User(db.model):
+    email=""
+    name=""
+    password=""
+    
+
+
+    def is_authenticated(self):
+        pass
+
+    def set_password(self, password):
+        pass
+
+    
