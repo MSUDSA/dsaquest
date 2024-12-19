282ff0ca09f5b35368ad3a1de0633c921bfbfa92
Oluwatobi <tobiolajide01@gmail.com>
2024-12-19 12:41:15 -0500

    all containers running

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+from flask_sqlalchemy import SQLAlchemy
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("POSTGRES_URL") 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = SQLAlchemy(app)
+
 
 
 @app.route('/')
 def index():
     return "Hello, world!"
 
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8081)
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("BACKEND_PORT"))
