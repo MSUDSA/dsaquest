d4b1d54f13b598d947557250d8d5b1b0dbf4d9ed
Oluwasegun-rm <segunsojijohn@gmail.com>
2024-12-19 14:13:38 -0500

    authentication changes

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,16 +1,24 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.model):
-    email=""
-    name=""
-    password=""
+
+
+
+
+
+class User(db.Model):
+    email = db.Column(db.String(40), unique = True, nullable = False, primary_key = True)
+    name = db.Column(db.String(40), unique = True, nullable = False)
+    password_hash = db.Column(db.String(128), nullable = False)
     
-
-
-    def is_authenticated(self):
-        pass
-
     def set_password(self, password):
-        pass
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"{self.name} is a user with {self.email} email."
+
 
     
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
-from app import create_app
+from app import create_app, db
+from app.models import User
+import os
+from flask import request
+
 
 app = create_app()
 
@@ -7,8 +11,33 @@ app = create_app()
 def index():
     return "Hello, world!"
 
+@app.route('/signup', method = ['POST'])
+def signup():
+    name, email, password = request.data["name"], request.data["email"], request.data["password"]
+    user = User.query.get(email)
+    if user:
+        return f"User already exists"
+    else:
+        user = User(name = name, email = email)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return f"Signup Success!"
+    
 
+@app.route("/login", method = ["POST"])
+def login():
+    email, password = request.data["email"], request.data["password"]
 
+    user = User.query.get(email)
+    if not user:
+        return f"User does not exist"
+    else:
+        valid = user.check_password(password)
+        if valid:
+            return repr(user)
+        else:
+            return f"Password incorrect"
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=os.getenv("BACKEND_PORT"))
+    app.run(debug=True, host="0.0.0.0", port= os.getenv("BACKEND_PORT"))
