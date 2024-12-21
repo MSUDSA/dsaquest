9b95ff9eb762e471774f91bcf0de6fe06537121c
Oluwatobi <tobiolajide01@gmail.com>
2024-12-21 17:24:31 -0500

    added streak feature

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -40,6 +40,7 @@ class User(db.Model):
         return {
             "email": self.email,
             "name": self.name,
+            "streak": self.streak,
             "last_login": self.last_login
             
         }
