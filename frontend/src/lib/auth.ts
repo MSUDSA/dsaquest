7d34cc46a79abc510957bc39c1c58312a1c6a52d
Oluwasegun Soji-John <149703204+Oluwasegun-rm@users.noreply.github.com>
2024-12-31 18:49:50 +0000

    Changes to login and Signup

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -4,24 +4,24 @@ interface userAuth {
     name?: string
 }
 
-export async function signUserIn({email, password} : userAuth){
-        const res = await fetch("http://localhost:8081/login", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({email, password})
-        })
-        return await res.json()
+export async function signUserIn({ email, password }: userAuth) {
+    const res = await fetch("http://localhost:8081/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password })
+    })
+    return await res.json()
 }
 
-export async function signUserUp({name, email, password}: userAuth) {
+export async function signUserUp({ name, email, password }: userAuth) {
     const res = await fetch("http://localhost:8081/signup", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({email, password, name})
+        body: JSON.stringify({ email, password, name })
     })
     return await res.json()
 }
