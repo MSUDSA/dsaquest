30162f34d9ff4863899bf93d6e901f3684b43e07
obwoj1 <149099282+obwoj1@users.noreply.github.com>
2025-01-02 12:50:44 -0500

    LANDING PAGE NOT GOOD

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,31 +1,30 @@
 import { useState } from "react";
-import Login from "../auth/login"
-import Signup from "../auth/signup"
-import { main } from "framer-motion/client";
-import LoginSignUp from "../components/authPages";
+import Login from "../auth/login";
+import Signup from "../auth/signup";
 
 const LandingPage = () => {
     const [showSignup, setShowSignup] = useState(false);
     const [showLogin, setShowLogin] = useState(false);
 
     return (
-        <main className="h-full ">
-            <div className=" h-full flex justify-center items-center">
-                <LoginSignUp></LoginSignUp>
-                {/* <button className = "" onClick={() => {
-                    setShowSignup(true)
-                    setShowLogin(false)
-                }}>Sign Up</button>
-                <button className= "" onClick={() => {
-                    setShowLogin(true)
-                    setShowSignup(false)
-                }}>Log In</button>
-                {showSignup ? <Signup /> : <></>}
-                {showLogin ? <Login /> : <></>} */}
+        <main className="h-full">
+            <div className="h-full flex flex-col justify-center items-center">
+                <h1>Welcome to DSAQUEST</h1>
+                <div>
+                    <button onClick={() => {
+                        setShowSignup(true);
+                        setShowLogin(false);
+                    }}>Sign Up</button>
+                    <button onClick={() => {
+                        setShowLogin(true);
+                        setShowSignup(false);
+                    }}>Log In</button>
+                </div>
+                {showSignup && <Signup />}
+                {showLogin && <Login />}
             </div>
         </main>
+    );
+};
 
-    )
-}
-
-export default LandingPage
+export default LandingPage;
