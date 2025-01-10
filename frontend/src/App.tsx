6686ae0f2a9472a65f5447004960774882898cc1
obwoj1 <149099282+obwoj1@users.noreply.github.com>
2025-01-10 16:47:48 -0500

    dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import Login from './auth/login'
+import Signup from './auth/signup'
+import Navbar from './components/Navbar'
 import './index.css'
+
 function App() {
 
   return (
 
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </Router>
   )
